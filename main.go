c7555f7291206e31ebd96d0068d2c8f47061ef6c
ForScoobySnacks <paszti.szabi@gmail.com>
2024-11-27 17:52:45 +0100

    Put, Patch functions

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var db *sql.DB
 func initDB() {
 	var err error
 
-	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/Films")
+	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/kakika")
 
 	if err != nil {
 		log.Fatal("Hiba az adatbázishoz való csatlakozáskor: ", err)
@@ -38,7 +38,7 @@ func initDB() {
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
@@ -51,7 +51,7 @@ func enableCors(next http.Handler) http.Handler {
 }
 
 func getFilms(w http.ResponseWriter, _ *http.Request) {
-	rows, err := db.Query("SELECT * FROM Filmek")
+	rows, err := db.Query("SELECT * FROM filmek2")
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -84,7 +84,7 @@ func addFilm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := db.Exec("INSERT INTO Filmek (nev,tipus,ertekeles) VALUES (?, ?, ?)", film.Nev, film.Tipus, film.Ertekeles)
+	_, err := db.Exec("INSERT INTO filmek2 (nev,tipus,ertekeles) VALUES (?, ?, ?)", film.Nev, film.Tipus, film.Ertekeles)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -105,7 +105,7 @@ func deleteFilm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = db.Exec("DELETE FROM Filmek WHERE id = ?", id)
+	_, err = db.Exec("DELETE FROM filmek2 WHERE id = ?", id)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -126,7 +126,7 @@ func getFilm(w http.ResponseWriter, r *http.Request) {
 
 	var film Film
 
-	err = db.QueryRow("SELECT * FROM Filmek WHERE id = ?", id).Scan(&film.ID, &film.Nev, &film.Tipus, &film.Ertekeles)
+	err = db.QueryRow("SELECT * FROM filmek2 WHERE id = ?", id).Scan(&film.ID, &film.Nev, &film.Tipus, &film.Ertekeles)
 
 	if err == sql.ErrNoRows {
 		http.Error(w, "Film was not found", http.StatusNotFound)
@@ -139,6 +139,72 @@ func getFilm(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(film)
+}
+
+func putFilm(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedMovie Film
+
+	if err := json.NewDecoder(r.Body).Decode(&updatedMovie); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.Exec("UPDATE filmek2 SET nev = ?, tipus = ?, ertekeles = ? WHERE id = ?", updatedMovie.Nev, updatedMovie.Tipus, updatedMovie.Ertekeles, id)
+
+	if err != nil {
+		http.Error(w, "Failed to update a movie", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedMovie)
+}
+
+func patchFilm(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var patchData map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&patchData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	query := "UPDATE filmek2 SET"
+	params := []interface{}{}
+	counter := 0
+
+	for key, value := range patchData {
+		if counter > 0 {
+			query += ", "
+		}
+		query += key + " = ?"
+		params = append(params, value)
+		counter++
+	}
+
+	query += "WHERE id = ?"
+	params = append(params, id)
+
+	_, err = db.Exec(query, params...)
+	if err != nil {
+		http.Error(w, "Failed to update the movie", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(patchData)
 }
 
 func main() {
@@ -163,6 +229,10 @@ func main() {
 			getFilm(w, r)
 		case "DELETE":
 			deleteFilm(w, r)
+		case "PUT":
+			putFilm(w, r)
+		case "PATCH":
+			patchFilm(w, r)
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
